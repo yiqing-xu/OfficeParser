9283af0705cac14298f22dbea43b5a712568608b
xuyiqing <yiqing.xu1996@gmail.com>
2021-01-15 20:52:05 +0800

    init

---
+++ src/main/java/com/tools/OfficeParserApplication.java
@@ -0,0 +1,13 @@
+package com.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeParserApplication.class, args);
+    }
+
+}
